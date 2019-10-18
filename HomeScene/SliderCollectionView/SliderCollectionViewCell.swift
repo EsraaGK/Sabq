d0d2ee56e0e2d2a57044306295a751d2523a8e02
Esraa Mohamed <esraamohamed@Esraas-Mac.local>
2019-10-18 13:27:05 -0700

    slider cell ui

---
+++ HomeScene/SliderCollectionView/SliderCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  SliderCollectionViewCell.swift
+//  Sabq
+//
+//  Created by Esraa Mohamed on 10/18/19.
+//  Copyright © 2019 EsraaGK. All rights reserved.
+//
+
+import UIKit
+
+class SliderCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
